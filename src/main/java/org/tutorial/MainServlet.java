aea06265d2c301d558a1a9f270f214086b15a34e
Karole Godin <kgodin@enssat.fr>
2022-05-04 11:18:08 +0200

    HelloWorld

--- src/main/java/org/tutorial/MainServlet.java
+++ src/main/java/org/tutorial/MainServlet.java
@@ -1,7 +1,26 @@
 package org.tutorial;
 
-import jakarta.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet ("/")
 public class MainServlet extends HttpServlet {
 
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+
+        out.println("Hello World !");
+        
+        //aller sur la page localhost:8080/JEEProject/
+
+  }
 }
